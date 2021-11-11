86b7d4cc1a7cdb505bc7aaa4b40e9eb8a2b62704
Heejeong jung <hee46868@gmail.com>
2021-11-11 17:00:31 +0900

    list 에 대해 stream 을 사용해보기

--- src/main/java/racinggame/model/CarList.java
+++ src/main/java/racinggame/model/CarList.java
@@ -2,8 +2,8 @@ package racinggame.model;
 
 import com.sun.media.sound.InvalidFormatException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CarList extends CarListUtil{
 
@@ -39,11 +39,11 @@ public class CarList extends CarListUtil{
   }
 
   public int maxMoveCount() {
-    int maxNum = MAX_NUMBER_INIT;
-    for(Car car : carList) {
-      maxNum = Math.max(car.moveCount, maxNum);
-    }
-    return maxNum;
+    AtomicInteger maxNum = new AtomicInteger();
+    carList.stream().forEach(car -> {
+      maxNum.set(Math.max(maxNum.get(), car.moveCount));
+    });
+    return maxNum.get();
   }
 
   public List<String> maxMoveCarsName() {
