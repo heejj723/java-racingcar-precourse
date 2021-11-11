adabfe07118268f28487f4de331be34905a19ca7
Heejeong jung <hee46868@gmail.com>
2021-11-11 17:10:13 +0900

    중복된 단위 테스트 제거

--- src/test/java/racinggame/model/GamesTest.java
+++ src/test/java/racinggame/model/GamesTest.java
@@ -11,18 +11,6 @@ import org.junit.jupiter.api.Test;
 class GamesTest {
 
   @Test
-  @DisplayName("Games 생성 검증: 잘못된 자동차 이름")
-  void games_객체_생성_검증_잘못된_자동차_이름() {
-    // given
-    String invalidCarNames = "e3,213343";
-    // when // then
-    assertThatThrownBy(() -> {
-      Games.of(invalidCarNames, "4");
-    }).isInstanceOf(InvalidFormatException.class);
-
-  }
-
-  @Test
   @DisplayName("Games 생성 검증: 잘못된 경주 횟수")
   void games_객체_생성_검증_잘못된_경주_횟수() {
     // given
